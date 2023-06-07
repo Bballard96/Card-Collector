3726fffdcee92237830aba5e9b536ba51d3ca831
Brendan96 <Brendan.ballard96@yahoo.com>
2023-06-06 19:58:29 -0500

    add styling

--- main_app/views.py
+++ main_app/views.py
@@ -18,17 +18,17 @@ class Home(LoginView):
 
 class CardCreate(CreateView):
   model = Card
-  fields = '__all__'
+  fields = ['name', 'brand', 'description', 'price']
   success_url = '/cards/'
   
 
   def form_valid(self, form):
-    form.instance.user = self.request.user  # form.instance is the cat
+    form.instance.user = self.request.user 
     return super().form_valid(form)
   
 class CardUpdate(UpdateView):
   model = Card
-  fields = ['brand', 'description', 'price']
+  fields = ['name', 'brand', 'description', 'price']
 
 class CardDelete(DeleteView):
   model = Card
@@ -55,42 +55,29 @@ def card_index(request):
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in
       login(request, user)
       return redirect('card-index')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'signup.html', context)
-  # Same as: return render(request, 'signup.html', {'form': form, 'error_message': error_message})
 
   
 S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com/'
 BUCKET = 'brendan-card-collector'
 
 def add_photo(request, card_id):
-  # photo-file will be the "name" attribute on the <input type="file">
   photo_file = request.FILES.get('photo-file', None)
   if photo_file:
     s3 = boto3.client('s3')
-    # need a unique "key" for S3 / needs image file extension too
-		# uuid.uuid4().hex generates a random hexadecimal Universally Unique Identifier
-    # Add on the file extension using photo_file.name[photo_file.name.rfind('.'):]
     key = uuid.uuid4().hex + photo_file.name[photo_file.name.rfind('.'):]
-    # just in case something goes wrong
     try:
       s3.upload_fileobj(photo_file, BUCKET, key)
-      # build the full url string
       url = f"{S3_BASE_URL}{BUCKET}/{key}"
-      # we can assign to cat_id or cat (if you have a cat object)
       photo = Photo(url=url, card_id=card_id)
       # Remove old photo if it exists
       card_photo = Photo.objects.filter(card_id=card_id)
